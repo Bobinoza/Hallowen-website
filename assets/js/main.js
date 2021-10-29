9efbd0574342a3954319eed62025c9582ca86bfe
Bobinoza <marioguerra.motorolla@gmail.com>
2021-10-29 15:13:08 -0300

    second deploy

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,23 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*================ HOME SWIPER ===============*/
+
+
+let homeSwiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+});
+
+/*================ CHANGE BACKGROUND HEADER ===============*/
+
+
+/*================ NEW SWIPER ===========================*/
+
+/*================ SCROLL SECTIONS ACTIVE LINK ===================*/
