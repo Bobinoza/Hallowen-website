2d6b8843529f6d9f80161952cb3600c56d41c14e
Bobinoza <marioguerra.motorolla@gmail.com>
2021-10-29 19:13:17 -0300

    I added content to the home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
 /*============== SHOW MENU ============*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
@@ -22,7 +22,7 @@ if(navClose){
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
@@ -30,18 +30,25 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*================ HOME SWIPER ===============*/
- 
+
 let homeSwiper = new Swiper(".home-swiper", {
     spaceBetween: 30,
     loop: 'true',
 
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true
+        el: ".swiper-pagination",
+        clickable: true
     },
 });
 
 /*================ CHANGE BACKGROUND HEADER ===============*/
+
+function scrollHeader() {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 
 /*================ NEW SWIPER ===========================*/
